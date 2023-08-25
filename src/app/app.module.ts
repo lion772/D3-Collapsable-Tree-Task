ba4f83f1d3c8e36313729c27e239b1acf552ed1d
lion772 <williamlopes64@gmail.com>
2023-08-25 16:13:32 +0200

    render tree chart (in progress), add some style, create reference in template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CollapsibleTreeComponent } from './components/collapsible-tree/collapsible-tree.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CollapsibleTreeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/collapsible-tree/collapsible-tree.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CollapsibleTreeComponent } from './collapsible-tree.component';
+
+describe('CollapsibleTreeComponent', () => {
+  let component: CollapsibleTreeComponent;
+  let fixture: ComponentFixture<CollapsibleTreeComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [CollapsibleTreeComponent]
+    });
+    fixture = TestBed.createComponent(CollapsibleTreeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
